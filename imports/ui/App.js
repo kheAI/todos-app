3bf7074210e6bd4daa9527219ccb6b48853a5391
Kai Chew <kafechew@gmail.com>
2026-05-08 18:18:22 +0800

    5.1 Install and Setup State

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,6 +3,12 @@ import { TasksCollection } from "/imports/api/TasksCollection";
 import './App.html';
 import './Task.html';
 
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+Template.mainContainer.onCreated(function () {
+  this.state = new ReactiveDict();
+});
+
 Template.mainContainer.helpers({
   tasks() {
     return TasksCollection.find({}, { sort: { createdAt: -1, _id: -1 } });
@@ -23,5 +29,12 @@ Template.form.events({
     });      
 
     target.text.value = ''; // Clear form
+  }
+});
+
+Template.mainContainer.events({
+  "click #hide-completed-button"(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
   }
 });
