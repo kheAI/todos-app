e91ca40ba210bc97ec7309c05656409ccd27151a
Kai Chew <kafechew@gmail.com>
2026-05-08 18:04:36 +0800

    3.2 Handle the Submit Event

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,3 +7,20 @@ Template.mainContainer.helpers({
     return TasksCollection.find({}, { sort: { createdAt: -1, _id: -1 } });
   },
 });
+
+Template.form.events({
+  async "submit .task-form"(event) {
+    event.preventDefault(); // Prevent page reload
+
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert directly into the database from the client!
+    await TasksCollection.insertAsync({
+      text,
+      createdAt: new Date(),
+    });      
+
+    target.text.value = ''; // Clear form
+  }
+});
