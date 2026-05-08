09acd36b0790b504fd9f4c041b54c438d7591e64
Kai Chew <kafechew@gmail.com>
2026-05-08 19:03:14 +0800

    7.4 Secure the Seed Data

--- server/main.js
+++ server/main.js
@@ -2,28 +2,37 @@ import { Meteor } from "meteor/meteor";
 import { Accounts } from "meteor/accounts-base";
 import { TasksCollection } from "/imports/api/TasksCollection";
 import "../imports/api/TasksPublication";
-import "../imports/api/TasksMethod";
+import "../imports/api/TasksMethod"; 
 
 const SEED_USERNAME = 'meteorite';
 const SEED_PASSWORD = 'password';
 
-const insertTask = (taskText) =>
-  TasksCollection.insertAsync({ text: taskText });
-
 Meteor.startup(async () => {
-  // If the database is empty, add these tasks
-  if ((await TasksCollection.find().countAsync()) === 0) {
-    [
-      "Learn Meteor 3",
-      "Style with PicoCSS",
-      "Conquer the World",
-    ].forEach(insertTask);
-  }
-
+  // 1. Create the seed user if they don't exist
   if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
     await Accounts.createUser({
       username: SEED_USERNAME,
       password: SEED_PASSWORD,
     });
+  }
+
+  // 2. Fetch the user we just created (or already existed)
+  const user = await Accounts.findUserByUsername(SEED_USERNAME);
+
+  // 3. Seed tasks securely using the user's ID
+  if ((await TasksCollection.find().countAsync()) === 0) {
+    const defaultTasks = [
+      "Learn Meteor 3",
+      "Style with PicoCSS",
+      "Conquer the World",
+    ];
+    
+    for (const taskName of defaultTasks) {
+      await TasksCollection.insertAsync({ 
+        text: taskName, 
+        createdAt: new Date(),
+        userId: user._id 
+      });      
+    }
   }
 });
