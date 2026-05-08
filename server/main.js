56fe24118ff6cc77c267c4169150a1f87eccd847
Kai Chew <kafechew@gmail.com>
2026-05-08 17:59:53 +0800

    2.2 Initialize the Database

--- server/main.js
+++ server/main.js
@@ -1,5 +1,16 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/imports/api/TasksCollection"; // or "../api/TasksCollection"
 
-Meteor.startup(() => {
-  // code to run on server at startup
+const insertTask = (taskText) =>
+  TasksCollection.insertAsync({ text: taskText });
+
+Meteor.startup(async () => {
+  // If the database is empty, add these tasks
+  if ((await TasksCollection.find().countAsync()) === 0) {
+    [
+      "Learn Meteor 3",
+      "Style with PicoCSS",
+      "Conquer the World",
+    ].forEach(insertTask);
+  }
 });
